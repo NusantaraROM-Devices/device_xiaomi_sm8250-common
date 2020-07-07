de77f4211fa23d052b6f6b98aa014eba0cf620ea
LuK1337 <priv.luk@gmail.com>
2020-07-07 12:47:29 +0200

    sm8250-common: Adapt PowerShare from instantnoodle

--- kona.mk
+++ kona.mk
@@ -116,7 +116,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.ims.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/handheld_core_hardware.xml \
     vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
-    
+
+# PowerShare
+PRODUCT_PACKAGES += \
+    lineage.powershare@1.0-service.xiaomi_kona
+
 # Ramdisk
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
