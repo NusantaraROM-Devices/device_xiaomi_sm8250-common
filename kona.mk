4d9a79dd9686f0eb302e29cb1ace3f3f919214b7
PIPIPIG233666 <pig.priv@gmail.com>
2020-11-01 17:51:18 -0500

    sm8250-common: Split component-overrides to support pure AOSP/QTI BT stack
    Change-Id: Ia40a996ba5164ba8a6fc5dde0dfb3ef859af1477
    Signed-off-by: Albert I <kras@raphielgang.org>

--- kona.mk
+++ kona.mk
@@ -173,7 +173,8 @@ PRODUCT_PACKAGES += \
 
 # Component overrides
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
+    $(LOCAL_PATH)/configs/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
 
 # Dex
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
