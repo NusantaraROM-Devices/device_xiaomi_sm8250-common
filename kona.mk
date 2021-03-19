fe38bbe0d5abb283930b9d95093b19b4505820fc
goodmeow <harunbam3@gmail.com>
2021-03-19 20:33:47 +0700

    sm8250-common: build hwcomposer
    Change-Id: I6b8d44931deb3a011b4d33ffd4644fb96174f38a

--- kona.mk
+++ kona.mk
@@ -160,6 +160,7 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     libdisplayconfig.qti \
     libdisplayconfig.qti.vendor \
+    hwcomposer.kona \
     gralloc.kona \
     libqdMetaData \
     libsdmcore \
