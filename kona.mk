d32a17bfe22edaadd99abe48e069a0e97c582815
Harun Al Rasyid <harunbam3@gmail.com>
2021-03-29 14:31:53 +0700

    sm8250-common: Build necessary display packages for vendor
    Change-Id: Ic25f66f451486aa8b434677997540af9c09f4c08

--- kona.mk
+++ kona.mk
@@ -169,15 +169,28 @@ PRODUCT_PACKAGES += \
     libvulkan \
     memtrack.kona \
     vendor.display.config@1.5 \
-    vendor.display.config@1.11.vendor \
     vendor.display.config@2.0 \
+    vendor.display.config@1.0.vendor \
+    vendor.display.config@1.1.vendor \
+    vendor.display.config@1.2.vendor \
+    vendor.display.config@1.3.vendor \
+    vendor.display.config@1.4.vendor \
+    vendor.display.config@1.5.vendor \
+    vendor.display.config@1.6.vendor \
+    vendor.display.config@1.7.vendor \
+    vendor.display.config@1.8.vendor \
+    vendor.display.config@1.9.vendor \
+    vendor.display.config@1.10.vendor \
+    vendor.display.config@1.11.vendor \
     vendor.display.config@2.0.vendor \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
     vendor.qti.hardware.display.mapper@1.1.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
-    vendor.qti.hardware.display.mapper@4.0.vendor
+    vendor.qti.hardware.display.mapper@4.0.vendor \
+    vendor.qti.hardware.display.mapperextensions@1.0.vendor \
+    vendor.qti.hardware.display.mapperextensions@1.1.vendor
 
 # DRM
 PRODUCT_PACKAGES += \
