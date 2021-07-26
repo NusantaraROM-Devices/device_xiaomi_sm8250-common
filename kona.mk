517e3c93f5b51ddc157b7f2c5cd91e45bb875a4e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-26 17:07:41 +0200

    sm8250-common: audio: Add audio policy configuration file with ULL enabled
    * alioth has broken audio without raw, and thanks god it doesn't has crackling when ULL is used
    * [goodmeow] add a2dp_offload_disabled_qti with ull.

--- kona.mk
+++ kona.mk
@@ -125,11 +125,19 @@ else
 AUDIO_POLICY_VOLUMES_FILE := $(LOCAL_PATH)/audio/audio_policy_volumes.xml
 endif
 
+ifeq ($(TARGET_ENABLE_AUDIO_ULL),true)
+AUDIO_POLICY_CONFIGURATION_FILE := audio_policy_configuration_ull.xml
+AUDIO_POLICY_CONFIGURATION_A2DP_FILE := audio_policy_configuration_a2dp_offload_disabled_qti_ull.xml
+else
+AUDIO_POLICY_CONFIGURATION_FILE := audio_policy_configuration.xml
+AUDIO_POLICY_CONFIGURATION_A2DP_FILE := audio_policy_configuration_a2dp_offload_disabled_qti.xml
+endif
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_io_policy.conf \
-    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
-    $(LOCAL_PATH)/audio/audio_policy_configuration_a2dp_offload_disabled_qti.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration_a2dp_offload_disabled_qti.xml \
+    $(LOCAL_PATH)/audio/$(AUDIO_POLICY_CONFIGURATION_FILE):$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/$(AUDIO_POLICY_CONFIGURATION_A2DP_FILE):$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration_a2dp_offload_disabled_qti.xml \
     $(AUDIO_POLICY_VOLUMES_FILE):$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     $(LOCAL_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
     $(LOCAL_PATH)/audio/bluetooth_qti_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_qti_audio_policy_configuration.xml \
