15b320e9528f3732c6aa44421cc0a486206ecd27
Nauval Rizky <enuma.alrizky@gmail.com>
2021-08-13 20:17:49 +0700

    sm8250-common: parts: Don't apply thermal preset when GameMode is active
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: David Setiawan <valkry24@gmail.com>
    Signed-off-by: alanndz <alanndz@qmail.id>

--- parts/src/org/lineageos/settings/thermal/ThermalUtils.java
+++ parts/src/org/lineageos/settings/thermal/ThermalUtils.java
@@ -19,6 +19,7 @@ package org.lineageos.settings.thermal;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.SystemProperties;
 import android.os.UserHandle;
 
 import androidx.preference.PreferenceManager;
@@ -51,6 +52,7 @@ public final class ThermalUtils {
     private static final String THERMAL_STREAMING = "thermal.streaming=";
 
     private static final String THERMAL_SCONFIG = "/sys/class/thermal/thermal_message/sconfig";
+    private static final String PROP_GAME_MODE = "sys.performance.level";
 
     private SharedPreferences mSharedPrefs;
 
@@ -165,6 +167,13 @@ public final class ThermalUtils {
                 state = THERMAL_STATE_STREAMING;
             }
         }
-        FileUtils.writeLine(THERMAL_SCONFIG, state);
+
+        if (!isInGameMode()) {
+            FileUtils.writeLine(THERMAL_SCONFIG, state);
+        }
+    }
+
+    private boolean isInGameMode() {
+        return SystemProperties.getInt(PROP_GAME_MODE, -1) > 0;
     }
 }
