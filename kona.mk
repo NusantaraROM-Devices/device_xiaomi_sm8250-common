e0c3d83ee0adac22a8d6183717a0c9f454cbba9f
goodmeow <harunbam3@gmail.com>
2022-02-19 10:02:18 +0530

    sm8250-common: update AOSP NXP NFC HAL dependencies

--- kona.mk
+++ kona.mk
@@ -380,7 +380,8 @@ PRODUCT_PACKAGES += \
     libchrome.vendor \
     NfcNci \
     SecureElement \
-    Tag
+    Tag \
+    android.hardware.secure_element@1.2.vendor
 
 PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
