3ccadc5ed762155fa1e0ab899473e5cd8dca8e5b
goodmeow <harunbam3@gmail.com>
2022-03-23 21:38:01 +0530

    8250-common: wfd: build libdisplayconfig.system.qti

--- kona.mk
+++ kona.mk
@@ -231,6 +231,7 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     libdisplayconfig.qti \
+    libdisplayconfig.system.qti \
     libdisplayconfig.qti.vendor \
     libcomposerextn.qti \
     libsdedrm \
