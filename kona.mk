e2d8c49f3de01012c4aee392423599753d1b6573
goodmeow <harunbam3@gmail.com>
2022-04-02 20:40:11 +0530

    Revert "Revert "sm8250-common: Downrev IRadio HAL to 1.5""
    This reverts commit 0d43a617fd0b64ec2d2420f94d06ecc46e908f16.

--- kona.mk
+++ kona.mk
@@ -449,8 +449,8 @@ PRODUCT_PACKAGES += \
     libxml2
 
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.6.vendor \
-    android.hardware.radio.config@1.3.vendor \
+    android.hardware.radio@1.5.vendor \
+    android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor
 
 # Rootdir
