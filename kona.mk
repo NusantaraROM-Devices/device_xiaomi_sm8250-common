b5b99b564155f7fdd3ebf2b37194cfe505c1b1e8
goodmeow <harunbam3@gmail.com>
2022-04-10 09:41:18 +0530

    build android.hardware.power@1.2.vendor

--- kona.mk
+++ kona.mk
@@ -422,6 +422,7 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power@1.2.vendor \
     android.hardware.power-service-qti \
     android.hardware.power.stats@1.0-service.mock
 
