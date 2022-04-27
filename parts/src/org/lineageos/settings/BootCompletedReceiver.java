74ebb895615b6b73b9d247c830d1a54473d1e4d7
goodmeow <harunbam3@gmail.com>
2022-04-27 20:58:40 +0530

    sm8250-common: remove High Touch Polling Rate support from tree  * leave its sepolicy

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -24,7 +24,6 @@ import android.content.IntentFilter;
 import android.util.Log;
 
 import org.lineageos.settings.doze.DozeUtils;
-import org.lineageos.settings.touchsampling.TouchSamplingUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 
@@ -37,8 +36,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DozeUtils.checkDozeService(context);
-        TouchSamplingUtils.restoreSamplingValue(context);
         ThermalUtils.startService(context);
-        RefreshUtils.startService(context);        
+        RefreshUtils.startService(context);
     }
 }
