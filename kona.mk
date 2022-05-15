8185a032eb1f6a5a5d86e8d112bf594e258705ab
johnmart19 <johnivan19999@gmail.com>
2022-05-15 08:25:29 +0300

    sm8250-common: Implement support of vendor.qti.hardware.btconfigstore@2.0
    - Import vendor.qti.hardware.fm impl, declare to compile hal
    - Import com.dsi.ant impl, declare to compile hal
    - Declare to use vendor.qti.hardware.bluetooth_audio@2.0-1
    [goodmeow] trim duplicate packages

--- kona.mk
+++ kona.mk
@@ -98,7 +98,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.1-impl \
-    android.hardware.soundtrigger@2.3-impl
+    android.hardware.soundtrigger@2.3-impl \
+    android.hardware.bluetooth.a2dp@1.0
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
@@ -141,8 +142,21 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
+    vendor.qti.hardware.bluetooth_dun@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor \
+    vendor.qti.hardware.fm@1.0.vendor
+
+# Bluetooth Library Deps
+PRODUCT_PACKAGES += \
+    libbluetooth_audio_session \
+    libbthost_if.vendor \
+    libldacBT_bco.vendor
+
+# Bluetooth Ant+ Hal Deps
+PRODUCT_PACKAGES += \
+    com.dsi.ant@1.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
