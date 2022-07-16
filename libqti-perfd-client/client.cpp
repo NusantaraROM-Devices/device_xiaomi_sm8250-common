1637695e8a62159cfabdc01ad62996121eb76078
goodmeow <aarunalr@nusantararom.org>
2022-07-16 08:38:41 +0530

    Revert "sm8250-common: Create dummy libqti-perfd-client"
    This reverts commit 422656cff7fef15f6b4ce8a7e34869ccb241e8bb.

--- libqti-perfd-client/client.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdint.h>
-
-extern "C" void perf_get_feedback() {}
-extern "C" void perf_hint() {}
-extern "C" void perf_lock_acq() {}
-extern "C" void perf_lock_cmd() {}
-extern "C" void perf_lock_rel() {}
-extern "C" void perf_lock_use_profile() {}
