cb30d9bb9c0e68d733a733f87eca7f051b06d2a9
Omkar Chandorkar <forumomkar@gmail.com>
2022-08-09 04:41:08 +0530

    sm8250-common: wlan: Enable IEEE 802.11ax support
    IEEE80211AX config is enabled inside qcom proprietary on recent QC SOCs starting from msmnile (855+)
    So, add support for building IEEE80211AX support inside wpa_supplicant.
    
    Below commit enables 11ax configuration on kona
    https://git.codelinaro.org/clo/la/platform/vendor/qcom/wlan/-/commit/0113651d8ce088fe133fce9a608ba13d7fb4b8c3
    As lahaina hostapd configuration is based on kona hence, enable 11ax support on kona and lahaina.

--- kona.mk
+++ kona.mk
@@ -497,6 +497,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
+
+# Wifi Enable IEEE 802.11ax support
+WIFI_FEATURE_HOSTAPD_11AX := true
+
 # WiFi Display
 PRODUCT_PACKAGES += \
     libnl \
