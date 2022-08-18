8c279fc6f9e1a9c7ce5ccb1c27a03602f3742718
goodmeow <aarunalr@nusantararom.org>
2022-08-18 14:02:33 +0530

    sm8250-common: do not include gsi_keys that doesnt exist on T

--- kona.mk
+++ kona.mk
@@ -7,9 +7,6 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-# Include GSI keys
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
